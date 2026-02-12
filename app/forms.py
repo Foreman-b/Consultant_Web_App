9368113779c00c0d7c9c5e2413b4a5e49c25dbcb
Foreman-b <hamzat4life@gmail.com>
2026-02-12 11:26:23 +0100

    Create forms that collect data from user, consultant_profile, availability, booking, payment and review

--- app/forms.py
+++ app/forms.py
@@ -1,24 +1,25 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from .models import User, Consultant_Profile, Availability, Booking, Payment, Review
 
-class UserForm(forms.ModelForm):
+class UserRegisterForm(UserCreationForm):
     password = forms.CharField(widget=forms.PasswordInput)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'phone_number', 'password', 'role']
+        fields = ('username', 'email', 'first_name', 'last_name', 'phone_number', 'password1', 'password2',)
 
 class ConsultantProfileForm(forms.ModelForm):
 
     class Meta:
         model = Consultant_Profile
-        fields = ['specialization', 'bio', 'meeting_platform', 'meeting_link', 'is_active']
+        fields = ('specialization', 'bio', 'meeting_platform', 'meeting_link', 'is_active')
 
 class AvailabilityForm(forms.ModelForm):
 
     class Meta:
         model = Availability
-        fields = ['date', 'max_slot']
+        fields = ('date', 'max_slot')
         widgets = {
             'date': forms.DateInput(attrs={'type': 'date'}),
         }
@@ -27,19 +28,19 @@ class BookingForm(forms.ModelForm):
 
     class Meta:
         model = Booking
-        fields = ['reason_for_session']
+        fields = ('reason_for_session')
 
 class PaymentForm(forms.ModelForm):
 
     class Meta:
         model = Payment
-        fields = ['amount']
+        fields = ('amount')
 
 class ReviewForm(forms.ModelForm):
 
     class Meta:
         model = Review
-        fields = ['rating', 'comment']
+        fields = ('rating', 'comment')
         widgets = {
             'rating': forms.NumberInput(attrs={'min': 1, 'max': 5}),
         }
