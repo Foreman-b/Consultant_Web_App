f2a7f83825dbac553b35fb86557af55c549fc865
Foreman-b <hamzat4life@gmail.com>
2026-02-13 20:08:36 +0100

    Return my book dashboard and session form in my views and make only the authenticated user can book session with consultant and see what's on booking dashboard

--- app/urls.py
+++ app/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('book-dashboard/', views.book_dashboard, name='book-dashboard'),
+    path('book-session/<int:availability_id>/', views.book_session, name='book-session'),
+
 ]
