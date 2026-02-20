730344317ce52adc33f7a9f265fd852125adfa1c
Foreman-b <hamzat4life@gmail.com>
2026-02-20 18:54:24 +0100

    Keep my DJANGO_SECRET_KEY in .env file

--- consultant_web/settings.py
+++ consultant_web/settings.py
@@ -10,7 +10,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h%446avjc%w(54b5-0j-4%t*0@t@qju=xy6+hv(981ih=z^=4a'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
