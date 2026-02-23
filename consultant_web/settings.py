10e7c75ab7d3230682b04169e6c2683e854f79b5
Foreman-b <hamzat4life@gmail.com>
2026-02-23 16:00:05 +0100

    Add my allowed hosts back to settings

--- consultant_web/settings.py
+++ consultant_web/settings.py
@@ -15,8 +15,8 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-# 'https://consultant-web-app.onrender.com', 'consultant-web-app.onrender.com'
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://consultant-web-app.onrender.com', 'consultant-web-app.onrender.com']
+
 
 
 # Application definition
