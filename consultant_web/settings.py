afe0aee11cc9e283f12d9805812621a807cf8ab6
Foreman-b <hamzat4life@gmail.com>
2026-02-23 19:00:26 +0100

    Turn on debug and uncomment security headers for production environments

--- consultant_web/settings.py
+++ consultant_web/settings.py
@@ -15,8 +15,8 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-# 'https://consultant-web-app.onrender.com', 'consultant-web-app.onrender.com'
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://consultant-web-app.onrender.com', 'consultant-web-app.onrender.com']
+
 
 
 
@@ -154,15 +154,15 @@ REST_FRAMEWORK = {
 }
 
 
-# # SECURITY HEADERS
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-# SECURE_SSL_REDIRECT = True
-# SESSION_COOKIE_SECURE = True
-# CSRF_COOKIE_SECURE = True
-# SECURE_BROWSER_XSS_FILTER = True
-# SECURE_CONTENT_TYPE_NOSNIFF = True
-# X_FRAME_OPTIONS = 'DENY'
+# SECURITY HEADERS
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = 'DENY'
 
-# SECURE_HSTS_SECONDS = 31536000      # Standard value is 1 year (31536000 seconds)
-# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-# SECURE_HSTS_PRELOAD = True
+SECURE_HSTS_SECONDS = 31536000      # Standard value is 1 year (31536000 seconds)
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
