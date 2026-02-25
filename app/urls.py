264bea87be17132f325316a277f72e9a304b44db
Foreman-b <hamzat4life@gmail.com>
2026-02-25 12:14:39 +0100

    Allow url to repath so that profile picture can appear

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.urls import re_path
+from django.views.static import serve
 
 
 urlpatterns = [
@@ -37,6 +38,7 @@ urlpatterns = [
 
 ] 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
